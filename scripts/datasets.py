273dcde8ac5fb7be7e9059dbe6adc505257fa27e
Yiyu Ni <niyiyu@uw.edu>
2022-11-30 00:10:59 -0800

    add benchmark test using SIREN and RFFN

---
+++ scripts/datasets.py
@@ -0,0 +1,30 @@
+#
+#     torch dataset class for OOI DAS Compression Project
+#
+#     https://github.com/niyiyu/DASCompression
+#
+#     Yiyu Ni niyiyu@uw.edu
+#     Nov. 29, 2022
+#
+##############################################
+
+import torch 
+import numpy as np
+
+class OOIDASDataset(torch.utils.data.Dataset):
+    def __init__(self, inputs, outputs):
+        'Initialization'
+        self.inputs = inputs.astype(np.float32)
+        self.outputs = outputs.astype(np.float32)
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.outputs)
+
+    def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        X = self.inputs[index, :]
+        y = self.outputs[index, :]
+
+        return X, y

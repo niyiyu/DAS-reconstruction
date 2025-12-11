85f33ac91932f1c4ba95895fcfbe1d593812ee2b
Yiyu Ni <niyiyu@uw.edu>
2025-12-11 00:22:38 -0800

    update tutorial notebook from workshop
    Signed-off-by: Yiyu Ni <niyiyu@uw.edu>

--- scripts/utils.py
+++ scripts/utils.py
@@ -58,3 +58,64 @@ def estimate_size(model):
         buffer_size += buffer.nelement() * buffer.element_size()
 
     return param_size + buffer_size
+
+def akurtosis(sample, dt, win=3.0, rtmemory_list=None):
+    from obspy.realtime.rtmemory import RtMemory
+    # deal with case of empty trace
+    if not rtmemory_list:
+        rtmemory_list = [RtMemory(), RtMemory(), RtMemory()]
+    if np.size(sample) < 1:
+        return sample
+
+    npts = len(sample)
+
+    c_1 = dt / float(win)
+    a1 = 1.0 - c_1
+    c_2 = (1.0 - a1 * a1) / 2.0
+    bias = -3 * c_1 - 3.0
+
+    kappa4 = np.empty(npts, sample.dtype)
+
+    rtmemory_mu1 = rtmemory_list[0]
+    rtmemory_mu2 = rtmemory_list[1]
+    rtmemory_k4_bar = rtmemory_list[2]
+
+    if not rtmemory_mu1.initialized:
+        memory_size_input = 1
+        memory_size_output = 0
+        rtmemory_mu1.initialize(sample.dtype, memory_size_input,
+                                memory_size_output, 0, 0)
+
+    if not rtmemory_mu2.initialized:
+        memory_size_input = 1
+        memory_size_output = 0
+        rtmemory_mu2.initialize(sample.dtype, memory_size_input,
+                                memory_size_output, 1, 0)
+
+    if not rtmemory_k4_bar.initialized:
+        memory_size_input = 1
+        memory_size_output = 0
+        rtmemory_k4_bar.initialize(sample.dtype, memory_size_input,
+                                   memory_size_output, 0, 0)
+
+    mu1_last = rtmemory_mu1.input[0]
+    mu2_last = rtmemory_mu2.input[0]
+    k4_bar_last = rtmemory_k4_bar.input[0]
+
+    # do recursive kurtosis
+    for i in range(npts):
+        mu1 = a1 * mu1_last + c_1 * sample[i]
+        dx2 = (sample[i] - mu1_last) * (sample[i] - mu1_last)
+        mu2 = a1 * mu2_last + c_2 * dx2
+        dx2 = dx2 / mu2_last
+        k4_bar = (1 + c_1 - 2 * c_1 * dx2) * k4_bar_last + c_1 * dx2 * dx2
+        kappa4[i] = k4_bar + bias
+        mu1_last = mu1
+        mu2_last = mu2
+        k4_bar_last = k4_bar
+
+    rtmemory_mu1.input[0] = mu1_last
+    rtmemory_mu2.input[0] = mu2_last
+    rtmemory_k4_bar.input[0] = k4_bar_last
+
+    return kappa4
